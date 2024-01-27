92360ff43046ad7c1b62bba134846675c0fdef8e
asmaur <maur.a@outlook.com>
2024-01-27 14:57:11 -0300

    variables

---
+++ lesson02/variables.py
@@ -0,0 +1,25 @@
+# variáveis & comentários em python.
+nome = 7
+primeiro_nome = "meu primeiro"
+
+# nome01 = "nome 2"
+nome3 = "nome 3"
+
+"""comentario
+de 
+multiplas linhas.
+"""
+
+linha1 = "*          *"
+linha2 = "************"
+linha3 = " Bem-vindo! "
+linha4 = "  Pedro  "
+
+print(" ")
+print(linha2)
+print(linha1)
+print(linha3)
+print(linha4)
+print(linha1)
+print(linha2)
+print(" ")
