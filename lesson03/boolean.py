d4d056a1a59cc6809a770e2debc81d9eadee7492
asmaur <maur.a@outlook.com>
2024-01-27 14:57:48 -0300

    operators and boolean

---
+++ lesson03/boolean.py
@@ -0,0 +1,21 @@
+# boolean
+
+x = True
+y = False
+z = True
+
+print(not y)
+print(x and y)
+print(y and x)
+print(x or y)
+print(y or x)
+
+valor = 100
+print(" ")
+
+# if valor > 90:
+#     print("Eu estou com sorte!")
+# else:
+#     print("Que hasard!")
+
+print("Eu estou com sorte!") if valor > 90 else print("Que hasard!")
---
+++ lesson03/operators.py
@@ -0,0 +1,44 @@
+# operador de atribuição
+nome = "Pedro"
+numero = 12345
+nota = 7.5
+
+# operadores aritmeticos
+print(4 - 2)
+print(2+2)
+print(30/5)
+print(37/7)
+print(37//7)
+print(round(37/7))
+print(2**3)
+
+valor = 40
+valor += 1
+print(valor)
+
+valor -= 1
+print(valor)
+
+valor *=10
+print(valor)
+
+valor /= 10
+print(valor)
+
+valor = round(valor)
+print(valor)
+
+print("Olá " + "mundo!")
+print("Olá " * 3)
+
+# Operadores de comparação
+print(23 == 20)
+print(23==23)
+
+print(35 != 35)
+print(35 != 45)
+
+print(10 > 5)
+print(10 < 5)
+print(11 >= 11)
+print(11<=11)
